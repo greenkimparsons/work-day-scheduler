ea476905e7eae77ade4abd8761f5b560d2181e38
Green Kim <greenkimparsons@gmail.com>
2021-03-26 16:24:06 -0400

    reorganized html,  relinked css, have basic structure for local storage on js

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # work-day-scheduler
 
 # descriptions
+First started off with date and aligning elements in jumbotron was successful in setting it. Then tried to create a container to put time, input form, and save button. 
 
 # screenshot
--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,74 @@ function updateCurrentTime(){
 
 
 updateCurrentTime();
+
+//if current it's red
+//if green it's future
+//if grey, time has passed
+
+//dynamically change the color of the row depending on the time
+//two ways to do this 1. hardcode everything to start, then set the hour
+//2. dynamically generate the rows in a for loop
+var saveButton = $("#save-button");
+var inputArea = $("#planner-input");
+
+function savePlan() {
+    var usersPlan = localStorage.getItem(inputArea);
+  
+    if (!inputArea) {
+      return;
+    }
+  
+    usersPlan.textContent = inputArea;
+  }
+
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    var usersPlan = inputArea.value;
+  
+    if (usersPlan === "") {
+      displayMessage("success", "Added to planner");
+  
+      localStorage.setItem("usersPlan", usersPlan);
+      savePlan();
+    }
+  });
+
+
+  // Jonathan's gem store example
+  var timesAndTasks = $("#times-and-tasks");
+
+  var time = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",];
+
+  function makeRow() {
+    timesAndTasks.empty();
+      for(var i = 0; i < time.length; i++){
+          var newRow = $("<div>").insertRow(0); //create a new row in a div
+          var timeCell = $("<div>").insertCell(0); //create a cell div
+          timeCell.addClass("col-3 col-md-4 col-xl-4"); //adding class col-2 to time cell
+          var timeText = timeCell.text(time[i]); //prints time text 
+          timeCell.append(timeText); //printing time onto time cell 
+          newRow.append(timeCell);
+          newRow.addClass("col-8"); //adding class col-8 to new row
+          newRow.append(saveButton); //appending save button to new row
+          timesAndTasks.append(newRow); //appending the new row in the container
+      }
+  }
+
+  function init () {
+      var itemsFromStorage = JSON.parse(localStorage.getItem("times"));//saves after refresh
+      if (itemsFromStorage !== null) {
+          times = itemsFromStorage;
+      }
+      makeRow();
+  }
+
+  timesAndTasks.on("click", saveButton, function(){
+      console.log($(this).attr("data-index")); //you get the index of the item arr
+      var clickedIndex = $(this).attr("data-index"); //how to save the input at that index
+      localStorage.setItem("items", JSON.stringify(times));
+      makeRow();
+  });
+
+  init();

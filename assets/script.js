0434084cade2c085e49a8d6d8afc6a3cd53507ec
Green Kim <greenkimparsons@gmail.com>
2021-03-27 11:25:06 -0400

    changed html structure and js functions with tutor - JQuery isn't working

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,8 @@ function updateCurrentTime(){
     currentDateTimeSpan.text(moment().format("MMMM Do YYYY"));
 }
 
-
+var timeBlock = document.querySelectorAll(".time-block");
+console.log(timeBlock);
 updateCurrentTime();
 
 //if current it's red
@@ -14,8 +15,13 @@ updateCurrentTime();
 //dynamically change the color of the row depending on the time
 //two ways to do this 1. hardcode everything to start, then set the hour
 //2. dynamically generate the rows in a for loop
-var saveButton = $("#save-button");
-var inputArea = $("#planner-input");
+// var saveButton = $("#save-button");
+var saveButton = document.querySelector(".saveBtn");
+var textArea = document.querySelector(".description");
+console.log(textArea.value); //console.log to make sure it works 
+// var inputArea = $("#planner-input");
+var inputArea = document.querySelector("#planner-input");
+// console.log(inputArea.value);
 
 function savePlan() {
     var usersPlan = localStorage.getItem(inputArea);
@@ -31,13 +37,14 @@ saveButton.addEventListener("click", function(event) {
     event.preventDefault();
   
     var usersPlan = inputArea.value;
+    var keyID = "9";
+    var value = textArea.value;
+    // if (usersPlan === "") {
+    //   displayMessage("success", "Added to planner");
   
-    if (usersPlan === "") {
-      displayMessage("success", "Added to planner");
-  
-      localStorage.setItem("usersPlan", usersPlan);
-      savePlan();
-    }
+      localStorage.setItem(keyID, value);
+    //   savePlan();
+    // }
   });
 
 
@@ -46,34 +53,35 @@ saveButton.addEventListener("click", function(event) {
 
   var time = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",];
 
-  function makeRow() {
-    timesAndTasks.empty();
-      for(var i = 0; i < time.length; i++){
-          var newRow = $("<div>").insertRow(0); //create a new row in a div
-          var timeCell = $("<div>").insertCell(0); //create a cell div
-          timeCell.addClass("col-3 col-md-4 col-xl-4"); //adding class col-2 to time cell
-          var timeText = timeCell.text(time[i]); //prints time text 
-          timeCell.append(timeText); //printing time onto time cell 
-          newRow.append(timeCell);
-          newRow.addClass("col-8"); //adding class col-8 to new row
-          newRow.append(saveButton); //appending save button to new row
-          timesAndTasks.append(newRow); //appending the new row in the container
-      }
-  }
+//   function makeRow() {
+//     timesAndTasks.empty();
+//       for(var i = 0; i < time.length; i++){
+//           var newRow = $("<div>").insertRow(0); //create a new row in a div
+//           var timeCell = $("<div>").insertCell(0); //create a cell div
+//           timeCell.addClass("col-3 col-md-4 col-xl-4"); //adding class col-2 to time cell
+//           var timeText = timeCell.text(time[i]); //prints time text 
+//           timeCell.append(timeText); //printing time onto time cell 
+//           newRow.append(timeCell);
+//           newRow.addClass("col-8"); //adding class col-8 to new row
+//           newRow.append(saveButton); //appending save button to new row
+//           timesAndTasks.append(newRow); //appending the new row in the container
+//       }
+//   }
 
   function init () {
       var itemsFromStorage = JSON.parse(localStorage.getItem("times"));//saves after refresh
       if (itemsFromStorage !== null) {
           times = itemsFromStorage;
       }
-      makeRow();
+    //   makeRow();
   }
 
-  timesAndTasks.on("click", saveButton, function(){
+  timesAndTasks.on("click", saveButton, function(event){
+      event.preventDefault();
       console.log($(this).attr("data-index")); //you get the index of the item arr
       var clickedIndex = $(this).attr("data-index"); //how to save the input at that index
-      localStorage.setItem("items", JSON.stringify(times));
-      makeRow();
+      localStorage.setItem("number", textarea.value); //needs key value pair**
+    //   makeRow();
   });
 
   init();

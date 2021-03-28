ac4863472d5a8f17dee578b22395b30deb83ba94
Green Kim <greenkimparsons@gmail.com>
2021-03-27 23:58:33 -0400

    submitting hw

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # work-day-scheduler
 
 # descriptions
-First started off with date and aligning elements in jumbotron was successful in setting it. Then tried to create a container to put time, input form, and save button. 
+First started off with date and aligning elements in jumbotron was successful in setting it. Then tried to create a container to put time, input form, and save button. Jquery didn't seem to work so had to just use js. 
 
 # screenshot
+
+## Deployed Site
+[Deployed Site](https://greenkimparsons.github.io/codequiz/.)
+![screenshot](./assets/workday.png)
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 var currentDateTimeSpan = $("#current-date-time");
 // var currentDateTimeSpan = document.querySelector("#current-date-time");
 console.log(currentDateTimeSpan);
+var saveButton = document.querySelector(".saveBtn");
+var textArea = document.querySelector(".description");
+// var inputArea = $("#planner-input");
+
+
 
 function updateCurrentTime(){
     currentDateTimeSpan.text(moment().format("MMMM Do YYYY"));
@@ -18,28 +23,13 @@ updateCurrentTime();
 //two ways to do this 1. hardcode everything to start, then set the hour
 //2. dynamically generate the rows in a for loop
 // var saveButton = $("#save-button");
-var saveButton = document.querySelector(".saveBtn");
-var textArea = document.querySelector(".description");
-console.log(textArea.value); //console.log to make sure it works 
-// var inputArea = $("#planner-input");
-var inputArea = document.querySelector("#planner-input");
-// console.log(inputArea.value);
 
-function savePlan() {
-    var usersPlan = localStorage.getItem(inputArea);
-  
-    if (!inputArea) {
-      return;
-    }
-  
-    usersPlan.textContent = inputArea;
-  }
 
 saveButton.addEventListener("click", function(event) {
     event.preventDefault();
   
-    var usersPlan = inputArea.value;
-    var keyID = "9";
+    var usersPlan = textArea.value;
+    var keyID = timeBlock[i];
     var value = textArea.value;
     // if (usersPlan === "") {
     //   displayMessage("success", "Added to planner");
@@ -49,27 +39,26 @@ saveButton.addEventListener("click", function(event) {
     // }
   });
 
-
-
-  var timesAndTasks = $("#times-and-tasks");
-
   for(var i = 0; i < timeBlock.length; i++){
       var timeVar = timeBlock[i];
       console.log(timeVar);
   }
 
+  function savePlan() {
+    var usersPlan = localStorage.getItem(textArea.value);
+  
+    if (!usersPlan) {
+      return;
+    }
+  
+    usersPlan.textContent = usersPlan;
+  }
+
   function init () {
-      var itemsFromStorage = JSON.parse(localStorage.getItem("times"));//saves after refresh
+      var itemsFromStorage = JSON.parse(localStorage.getItem(textArea));//saves after refresh
       if (itemsFromStorage !== null) {
           times = itemsFromStorage;
       }
   }
-
-  timesAndTasks.on("click", saveButton, function(event){
-      event.preventDefault();
-      console.log($(this).attr("data-index")); //you get the index of the item arr
-      var clickedIndex = $(this).attr("data-index"); //how to save the input at that index
-      localStorage.setItem("number", textarea.value); //needs key value pair**
-  });
 
   init();
